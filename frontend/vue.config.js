16e563c346d2987b225691a8fabbf29ed5b1d504
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-15 12:21:10 -0500

    get rid of the semicolons

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,9 +8,9 @@ module.exports = {
         changeOrigin: true,
         secure: false,
         pathRewrite: { '^/api/v1': '/api/v1' },
-        logLevel: 'debug',
-      },
-    },
+        logLevel: 'debug'
+      }
+    }
   },
   chainWebpack: (config) => {
     // markdown Loader
@@ -28,7 +28,7 @@ module.exports = {
       .loader('./markdown-routes-loader.js')
       .end()
       .use('ejs-loader')
-      .loader('ejs-loader');
+      .loader('ejs-loader')
     // .tap(options => {
     //   console.log("OPTIONS",options);
     //   return {
@@ -43,9 +43,9 @@ module.exports = {
       sass: {
         implementation: require('sass'),
         sassOptions: {
-          indentedSyntax: false,
-        },
-      },
-    },
-  },
-};
+          indentedSyntax: false
+        }
+      }
+    }
+  }
+}
