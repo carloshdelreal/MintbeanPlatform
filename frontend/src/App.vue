721653238cd9ff58a49ed47b07b637a7f1c47270
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-15 17:44:41 -0500

    introduce error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,7 +19,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 .accent-bar {
   position: fixed;
